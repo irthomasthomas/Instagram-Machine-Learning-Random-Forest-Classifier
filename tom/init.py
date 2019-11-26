ed3b58ae00a8e17884a9da58eae91bbb1bf8d2f9
ir.thomasthomas@gmail.com <ir.thomasthomas@gmail.com>
2019-11-26 14:32:35 +0000

    update redis opp

--- tom/init.py
+++ tom/init.py
@@ -11,9 +11,17 @@ with open('/root/dev/projects/scrape/unbalanced4.6.onnx', 'rb') as f:
     res = conn.execute_command('AI.MODELSET', 'auction:model', 'ONNX', 'CPU', model)
     print(res)
 
+initialized_key = '{}:initialized'.format('post:')
+# Check if this Redis instance had already been initialized
+initialized = conn.exists(initialized_key)
+if initialized:
+    print('Discovered evidence of a privious initialization - skipping.')
+    exit(0)
 # Load the gear    
 print('Loading gear - ', end='')
 with open('gear.py', 'rb') as f:
     gear = f.read()
     res = conn.execute_command('RG.PYEXECUTE', gear)
     print(res)
+print('Flag initialization as done - ', end='') 
+print(conn.set(initialized_key, 'most certainly.'))
