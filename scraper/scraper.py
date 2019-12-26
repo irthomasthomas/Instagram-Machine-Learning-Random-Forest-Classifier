def7a339f339329aca3749c959127f001368be66
irthomasthomas <ir.thomasthomas@gmail.com>
2019-12-26 16:17:35 +0000

    Redis

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,9 +9,9 @@ while True:
     tag = r.brpop('tagsin')[1]
     print(f'Received request for {tag}')
     with scraper:
-        for count in scraper.get_hashtag_posts(tag, resume=True):
+        for count in scraper.get_hashtag_posts(
+            tag, resume=True):
             total += count
             print(f'scraped: {total}')
-            if total > 200:
-                total = 0
+            if total > 170:
                 break
