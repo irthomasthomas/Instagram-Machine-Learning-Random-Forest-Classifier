f9ee66f3977eac5a18ffd3276dacde8efb5dec5e
irthomasthomas <ir.thomasthomas@gmail.com>
2020-01-06 22:40:33 +0000

    Refactoring/Caching/Deep-Scrape  Scrape:Loader         - Handle deep scan background scraping         - Resume from redis key  redis_cache_maker         - Cache front page from topk:top100tags         - Cache page if stream has results + ArchiveScrape as scraper_bg_archiver.py         - Scrapes 10 pages then lpush         - Till all posts are retrieved

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,25 +6,43 @@ from multiprocessing import Process
 r = redis.Redis(decode_responses=True)
 
 scraper = InstaloaderTommy()
+# TODO: DECIDE RELEVANCE BASED ON 1 PAGE SCRAPE AND RELEVANT
+# IF NOT RELEVANT: msg: Sorry this tag doesn't have much stuff..
+# ... here is some related stuff...
+# return related tags
+
+# TODO: RELATED RESULTS
 # TODO: CHECK TIME SINCE SCRAPE
 # TODO: ADD ALL BLOOM / CUCKOO FILTER
+# TODO: CHECK HASHTAG/POSTID LIST AND STOP ON DUPLICATE
+# TODO: CHECK AGAINST GLOBAL LIST AND RETRIEVE PREDICTION
+# TODO: GEAR CHECK FOR EXISTING PREDICTION
 
-def scrape(tag):
+def scrape(tag, num_to_scrape):
+    # TODO: NAMESPACE so I can do eg TODO:redis:BigOFunc
     r.delete(f'scrape:finished:{tag}')
     with scraper:
         total = 0
-        for count in scraper.get_hashtag_posts(
-            tag, resume=True):
-            total += count
-            print(f'scraped: {total}')
-            if r.xlen(f'tags:out:{tag}') > 5:
-                r.lpush('cache:queue:ready', tag)
-            if total > 600:
-                total = 0
-                res = r.lpush('cache:queue:ready', tag)
-                print(f'push cache:queue:ready: {res}')
-                break
+        # TODO: check dump
+        try:
+            for count in scraper.get_hashtag_posts(
+                    tag, dump_page=True, resume=False,
+                    duplicate_check=True):
+                # TODO:Should I control scraping here or in class?
+                total += count
+                # print(f'scraped: {total}')
+                added = r.sadd('set:cache:queue', tag)
+                if added:
+                    r.lpush('cache:queue:ready', tag)
 
+                if total > num_to_scrape:
+                    total = 0
+                    # TODO: DONE: ONLY CACHE READY WHEN STREAM OUT > 10
+                    # TODO: DONE: ADD TO SET IF SUCCESS LPUSH
+                    return
+        except:
+            return
+# TODO: REWRITE IN CRYSTAL
 # TODO: STOP SCRAPING WHEN DUPLICATE FOUND
 # CUCKOO FILTER?
 # store every prediction. if exists retrieve prediction else predict.
@@ -32,7 +50,24 @@ def scrape(tag):
 
 while True:
     print('Ready! Waiting for a hashtag from redis...')
-    tag = r.brpop('tagsin')[1]
+    # TODO: background scraping. pop a tag and resume page and total
+    # TODO: SUBSCRIBE AND BARK
+    # TODO: BACKGROUND SCRAPE HISTORIC
+        
+    tag = r.brpop('list:tagsin')[1] # add rootTag
+    r.srem('tagsin', tag)
+    root_tag_key = f'rootTag:{tag}'
+    root_tag = r.get(root_tag_key)
+    print(f'root_tag: {root_tag}')
+    if root_tag == tag or 'None':
+        print(f'rootTag: {root_tag}')
+        # this is the root tag req so scrape everything ..
+        # and look for related
+        num_to_scrape = 600
+    else:
+        print(f'found root_tag: {root_tag}')
+        print(f'scraping 300 posts')
+        num_to_scrape = 30
     key = f'scraped:recent:{tag}'
     scraped = r.get(key)
     print(f'SCRAPE REQ: {tag}')
@@ -40,13 +75,17 @@ while True:
     if scraped:
         print(f'FOUND SCRAPED_RECENT KEY')
         continue
-    start = time.perf_counter()
-    p = Process(target=scrape, args=(tag,))
+    else:
+        r.set(key, "True")
+    p = Process(target=scrape, args=(tag, num_to_scrape))
     p.start()
-    end = time.perf_counter()
-    print(f'TIMER: {end - start}')
-    res = r.set(key, "True")
     r.expire(f'scraped:recent:{tag}', 600)
 
-# TODO: check if tag has been recently scraped.
-    
+# TODO: IF ARCHIVAL SCAN DON'T STOP
+# TODO: Fan out search
+
+# TODO: RACE Proxies
+# TODO: Find related tags results
+# TODO: CREATE AN INDEX TAG > POST
+# TODO: CHECK IF ID EXIST AND STOP SCRAPING
+# TODO: Dump page to redisjson
--- tom/gear.py
+++ tom/gear.py
@@ -1,28 +1,36 @@
 import redisAI as rai
-import redis
+from redis import Redis
 from stop_words import get_stop_words
 import re
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 import numpy as np
+import time
 
 vectorizer = pickle.load(open("/home/tommy/development/vectorizer.pickle", "rb"))
-conn = redis.Redis(host="localhost", port=6379, db=0)
+conn = Redis(host="localhost", port=6379, db=0)
+stopwords = get_stop_words('english')
 
 
 def pre_proc_text(x):
 
-    def extract_hashtags(caption):
+    def extract_hashtags(caption, postId):
         regexp = re.compile(r"(?:#)(\w(?:(?:\w|(?:\.(?!\.))){0,28}(?:\w))?)")
-        tags = []
+        # tags = []
 
         def repl(m):
-            tags.append(m.group(0)[1:])
+            tag = m.group(0)[1:]
+            # tags.append(t)
+            # TODO: INSERT TAGS INTO REDIS LIST HERE
+            # DELETE THE LIST LATER IF NOT REQUIRED
+            # TODO: GEARS PIPELINE
+            execute('SADD', f'post:tags:{postId}', tag)
+            execute('SADD', f'tag:posts:{tag}', postId)
             return ""
 
         caption = regexp.sub(repl, caption.lower())
-        return caption, tags
+        return caption
 
     def extract_mentions(caption):
         regexp = re.compile(r"(?:@)(\w(?:(?:\w|(?:\.(?!\.))){0,28}(?:\w))?)")
@@ -40,89 +48,114 @@ def pre_proc_text(x):
         return text.translate(translator)
 
     def tokenize(text):
+        # TODO: Anomalous backslash in string
         text = re.split('\W+', text)
         return text
 
     def remove_stopwords(text, stopword):
         text = [word for word in text if word not in stopword]
         return text
-
+    
+    # TODO: BREAK ON DUPE
+    postId = x['postId']
+    rootTag = x['rootTag']
     caption = x['caption']
-    stopwords = get_stop_words('english')
-    caption, tags = extract_hashtags(caption)
 
-    caption = extract_mentions(caption)
+    execute('cmsIncrBy', 'sketch:allposts', postId, '1')
+    execute('cmsIncrBy', f'sketch:all:{rootTag}', postId, '1')
+
+
+    caption = extract_hashtags(caption, postId)
+
+    
+    caption_text = extract_mentions(caption)
+    caption = caption_text
+
+
     caption = remove_punct(caption)
     caption = tokenize(caption)
+
+
     caption = remove_stopwords(caption, stopwords)
     caption = ' '.join(caption)
     caption = caption.rstrip()
+
+    # print(f'PRE PROCESSOR TIME: {end - start}')
+    # print(f't1: {end2 - start} t2: {end3 - end2} t3: {end4 - end3}')
+
+    # print(f'cap len: {len(caption)}') #not len slow 
+    # print(f'tags len: {len(tags)}')
     # print(caption)
+    # TODO: DON'T RETURN TAGS. THEY'RE NOW IN REDIS LIST
     
-    return x, caption, tags
+    return x, caption, caption_text
 
-
-# def runModel(x):
-    #     ''' run onnx model '''
-    #     print(str(x))
-    #     print('model start')
-    #     input_name = 'float_input'
-    #     label_name = 'output_label'
-    #     vectorizer = pickle.load(open("/etc/tommy/vectorizer.pickle", "rb"))
-
-    #     # tensor = redisAI.createTensorFromValues(
-    #     #     'FLOAT', 1, 80)
-    #     modelRunner = rai.createModelRunner('auction:model')
-    #     # text = x['caption']
-    #     text = x
-    #     X = vectorizer.transform([text]).toarray()
-    #     print("vectorized")
-        
-    #     X_ba = bytearray(X.tobytes())
-    #     # tensor = rai.createTensorFromValues('INT32', [1, 80], X_ba)
-    #     tensor = rai.createTensorFromBlob('FLOAT', [1, 80], X_ba)
-    #     rai.modelRunnerAddInput(modelRunner, input_name, tensor)
-    #     rai.modelRunnerAddOutput(modelRunner, label_name)
-    #     # rai.modelRunnerAddOutput(modelRunner, prob_label)
-
-    #     model_replies = rai.modelRunnerRun(modelRunner) #error expected 2 outputs got one
-    #     model_output = model_replies[0]
-    #     print(str(model_output))
-
-# Todo: asd
-
-def runModel2(x):
-    # print(f'runModel: {x}')
+def runModel(x):
+    # start = time.perf_counter()
     caption = x[1]
     sample = vectorizer.transform([caption]).toarray()
     ba = np.asarray(sample, dtype=np.float32)
-    conn.execute_command(
+    execute(
         'AI.TENSORSET', 'auction:tensor', 'FLOAT',
         '1', '80', 'BLOB', ba.tobytes())
-    conn.execute_command(
+    execute(
         'AI.MODELRUN', 'auction:model',
         'INPUTS', 'auction:tensor',
         'OUTPUTS', 'out_label', 'out_probs')
-    out = conn.execute_command(
+    out = execute(
         'AI.TENSORGET', 'out_label', 'VALUES')
+    # end = time.perf_counter()
+    # print(f'NEW: {end - start}')
+    # print(out)
     return out[2][0]
-
 
 def storeResults(x):
     ''' store to output stream '''
-    tag = x[0]['rootTag']
-    streamKey = 'tags:out:' + tag
-    execute('SADD', 'trackedTags', tag)
+    # start = time.perf_counter() # time 0.0001
+    # print(len(x[2]))
+    # TODO: PIPELINE
+    rootTag = x[0]['rootTag']
+    postId = x[0]['postId']
     shortcode = x[0]['shortcode']
+    streamKey = 'tags:out:' + rootTag
     link = f'https://www.instagram.com/p/{shortcode}'
+
+    # execute('SADD', 'trackedTags', rootTag)
     execute('XADD', streamKey,
-        'MAXLEN', '~', 5000, '*',
-        'streamId', x[0]['streamId'],
+        'MAXLEN', '~', 1000, '*',
+        'postId', x[0]['postId'],
         'imgUrl', x[0]['imgUrl'],
-        'link', link) 
-    execute('topk.add', 'top10tags', tag)
-    # key = 'post:tags:' + str(x[0]['postId'])
-    # redis.lpush('foo', *[1,2,3,4,5,6,7,8,9]) # https://stackoverflow.com/questions/15850112/redis-how-to-parse-a-list-result
+        'link', link,
+        'rootTag', rootTag,
+        'likes', x[0]['likes'],
+        'comments_count', x[0]['comments'],
+        'caption_text', x[3],
+        'typename', x[0]['typename'],
+        'owner_id', x[0]['owner_id'],
+        'ig_timestamp', x[0]['timestamp'],
+        'retrieved_date', x[0]['scrape_date'])
+    
+    # TODO: IF STREAM LEN > 10 SET CACHE FLAG
+    # end1 = time.perf_counter()
+    # print(f'Timer1: {end1 - start}')
+
+    # TODO: REFACTOR AND MOVE THIS OUT OF THE GEAR
+    # AND IN TO THE CACHE HANDLER
+
+    # TODO: GEAR SHOULD NOTIFY CACHE MAKER
+    # TODO: CACHE MAKER DECIDES IF ANYTHING TO CACHE
+    execute('topk.add', 'top10tags', rootTag)
+    execute('topk.add', 'top100tags', rootTag)
+    execute('zincrby', 'topzhashtags:', rootTag, 1)
+    execute('cmsIncrBy', f'sketch:out:{rootTag}', postId, '1')    
+    # for tag in x[2]:
+    #     execute('SADD', f'post:tags:{postId}', tag)
+    #     execute('SADD', f'tag:posts:{tag}', postId)
+    # Add The stream key to a stream
+    # block read 1st stream
+    # get stream key
+    # end2 = time.perf_counter()
+    # print(f'timer2: {end2 - end1}')
 
 def printx(x):
     print(f'X: {x}')
@@ -130,7 +163,10 @@ def printx(x):
 
 gb = GearsBuilder('StreamReader')
 gb.map(pre_proc_text)
-gb.filter(runModel2)
+gb.filter(runModel)
 gb.foreach(storeResults)
 gb.register('post:')
-# TODO: add a key when there are 10+ results 
+
+# TODO: look for words in sets 
+# E.G. "FOR SALE" "SOLD" "BUY NOW" "BID" "DIBS"
+# IF IT EXISTS STORE THEM
