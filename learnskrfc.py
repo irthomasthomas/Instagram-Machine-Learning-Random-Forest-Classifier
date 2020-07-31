0aeba60b3f6543cc06a843ec302695cf027419ec
root <root@apocalypse.ecsta.tk>
2020-07-31 04:44:42 -0700

    Updated to allow batch infer in onnx

--- learnskrfc.py
+++ learnskrfc.py
@@ -93,8 +93,6 @@ def train_sk_rfc(input_file, stop_words):
         document = ' '.join(document)
         document = document.rstrip()
         documents.append(document)
-    # print(df)
-    # Convert to numbers with bag of words
 
     vectorizer = TfidfVectorizer(
         max_features=80, stop_words=stop_words)
@@ -127,11 +125,11 @@ def train_sk_rfc(input_file, stop_words):
     
     print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
-    print(accuracy_score(y_test, y_pred))
+    print(f"accuracy score: {accuracy_score(y_test, y_pred)}")
     # print('Mean Absolute Error:', mean_squared_error(y_test, y_pred))
     # print('Mean Squared Error: ', mean_squared_error(y_test, y_pred))
     # print('Root Mean Squared Error: ', np.sqrt(mean_squared_error(y_test, y_pred)))
-    # cross_validate(X_train, y_train, classifier)
+    cross_validate(X_train, y_train, classifier)
     with open("text_classifier2020", "wb") as picklefile:
         pickle.dump(classifier, picklefile)
         
@@ -139,10 +137,11 @@ def train_sk_rfc(input_file, stop_words):
 
 
 def save_sklearn_to_onnx(classifier, output_filename):
-    # Convert to ONNX format 
+    # Convert to ONNX format
     from skl2onnx import convert_sklearn
     from skl2onnx.common.data_types import FloatTensorType
-    initial_type = [('float_input', FloatTensorType([1, 80]))]
+    # initial_type = [('float_input', FloatTensorType([1, 80]))]
+    initial_type = [('float_input', FloatTensorType([None, 80]))]
     onx = convert_sklearn(classifier, initial_types=initial_type)
     with open(output_filename, "wb") as f:
         f.write(onx.SerializeToString())
@@ -155,118 +154,77 @@ def load_model_onnx(model_file):
 
 
 def predict_and_merge(input_file, output_file, sess):
-
-    df = pd.read_csv(input_file, header=None)
-    df.columns = ["data", "target"]
-
-    df, y = df.data, df.target
+    timer1 = time.time()
+    print("PREDICT AND MERGE")
+    df = pd.read_csv(input_file, header=1)
+    df.columns = ["MOD_CAP", "TRAIN", "FULL_CAP"]
+    df, y = df.FULL_CAP, df.TRAIN
 
     test_documents = []
     for caption in range(0, len(df)):
         document = str(df[caption])
+        document = pre_proc_text(document)
+        document = remove_stopwords(document, stop_words)
+        document = ' '.join(document)
+        document = document.rstrip()
+
         test_documents.append(document)
 
-    vectorizer = pickle.load(open("vectorizer.pickle", "rb"))
+    vectorizer = pickle.load(open("vectorizer2020.pickle", "rb"))
 
     X = vectorizer.transform(test_documents).toarray()
 
     input_name = sess.get_inputs()[0].name
     label_name = sess.get_outputs()[0].name
+    # pred_onx = sess.run(
+        # [label_name], {input_name: X.astype(np.float32)})[0]
+    # stimeronnx = time.time()
     pred_onx = sess.run(
-        [label_name], {input_name: X.astype(np.float32)})[0]
+        None, {input_name: X.astype(np.float32)})[0]
+    print("pred_onx")
     print(str(pred_onx))
+    # etimeronnx = time.time() - stimeronnx
+    # print(f"onnx pred time: {etimeronnx}")
 
-    with open("text_classifier", "rb") as training_model:
+    with open("text_classifier2020", "rb") as training_model:
         model = pickle.load(training_model)
-
-    # prediction = pd.DataFrame(pred_onx)
+    # stimerog = time.time()
     prediction = model.predict(X)
-    # prediction = pd.DataFrame(model.predict(X))
+    # etimerog = time.time() - stimerog
+    # print(f"og model time: {etimerog}")
+    # timer2 = time.time()
+    # print(f"time1: {timer2 - timer1}")
 
     print(confusion_matrix(y, prediction))
     print(classification_report(y, prediction))
-    print(accuracy_score(y, prediction))
+    print(f"accuracy_score: {accuracy_score(y, prediction)}")
+    # timer3 = time.time()
 
     pred_df = pd.DataFrame()
 
-    pred_df['data'] = df
+    pred_df['caption'] = df
     pred_df['target'] = y
     pred_df['prediction'] = prediction
     pred_df['onnx'] = pred_onx
 
     pred_df.to_csv(output_file, header=True)
-
+    # timer4 = time.time()
+    # print(f"timer2: {timer4 - timer3}")
 
 def predict(text, onx):
-    # vectorizer = pickle.load(open("/etc/tommy/vectorizer.pickle", "rb"))
     vectorizer = pickle.load(open("vectorizer2020.pickle", "rb"))
-    # vectorizer = pickle.load(open("vectorizer.pickle", "rb"))
-    # stop_words = get_stop_words('english')
 
-    # vectorizer = TfidfVectorizer(
-    #     max_features=80, stop_words=stopwords.words('english'))
-    print(type(text))
     input_name = onx.get_inputs()[0].name
     label_name = onx.get_outputs()[0].name
-    print(input_name)
-    print(label_name)
-    # ERRORS HERE    
+
     X = vectorizer.transform(text).toarray()
     pred_onx = onx.run(
         [label_name], {input_name: X.astype(np.float32)})[0]
     print(pred_onx)
-    # with open("text_classifier", "rb") as training_model:
-    # # #     model = pickle.load(training_model)
-    # prediction = model.predict(X)
-    # print(str(prediction))
-
 
 def cross_validate(X_train, y_train, clf):
     from sklearn.model_selection import cross_val_score
-    # from sklearn.datasets import fetch_covtype
-    # from sklearn import grid_search
     print(np.mean(cross_val_score(clf, X_train, y_train, cv=10)))
-
-
-def train_models(input_file, test_file):
-    from sklearn.model_selection import train_test_split
-    from sklearn.svm import SVC, LinearSVC, NuSVC
-    from sklearn.metrics import accuracy_score, classification_report
-    
-    train = pd.read_csv(input_file, header=None)
-    test = pd.read_csv(test_file, header=0)
-    train.columns = ["data", "target"]
-    print(train.describe())
-    
-    train, y = train.data, train.target
-
-    documents = []
-    for caption in range(0, len(train)):
-        document = str(train[caption])
-        documents.append(document)
-    vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'))
-    
-    X = vectorizer.fit_transform(documents).toarray()
-    
-    # features = train.iloc[:,0:1]
-    # labels = ["data", "target"]
-
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     X, y, test_size=0.2, random_state=0)
-
-    x1, x2, y1, y2 = train_test_split(
-        X, y, random_state=0, test_size=0.2
-        )
-    print(x1.shape)
-    print(x2.shape)
-    print(y1.shape)
-    print(y2.shape)
-    
-    LSVC = LinearSVC()
-    LSVC.fit(x1,y1)
-    y2_LSVC_model = LSVC.predict(x2)
-    print("LSVC Accuracy :", accuracy_score(y2, y2_LSVC_model))
-
 
 # take command of your life. cultivate self discipline.
     # output_file = "output2.csv" # 55k rows/4744 tagged id| og_caption| mod_caption| target
@@ -278,45 +236,55 @@ def train_models(input_file, test_file):
     # input_file = "ml data - full - balanced.csv"  # 7000 rows cleaned and cat  
     # input_file = "mlOutput - testSet.csv" # 700 rows cleaned and categorised
     # input_file = "~/mldata/ml data - full - biased - testSet.csv" # 1000 rows cleaned and cat
-input_file = "~/mldata/training_captions.csv"
+
+time0 = time.time()
 stop_words = get_stop_words('english')
-print(type(stop_words))
-print(stop_words)
 new_stop_words = []
 for word in stop_words:
     new_stop_words.append(pre_proc_text(word)[0])
 stop_words = new_stop_words
 new_stop_words = 0
-print(type(stop_words))
-print(stop_words)
-classifier = train_sk_rfc(input_file, stop_words)
-save_sklearn_to_onnx(classifier, "testclf72020.onnx")
-# train_models(input_file, test_file)
-sess = load_model_onnx("testclf72020.onnx")
-# sess = load_model_onnx("model.onnx")
 
-# sess = load_model_onnx('~/mldata/unbalanced4.6.onnx')
+time01 = time.time()
+print(f"time01: {time01 - time0}") # 0.019
+
+input_file = "~/mldata/training_captions.csv"
+classifier = train_sk_rfc(input_file, stop_words)
+
+time02 = time.time()
+print(f"time02: {time02 - time01}") # 60s
+
+save_sklearn_to_onnx(classifier, "testclf72020.onnx")
+sess = load_model_onnx("testclf72020.onnx")
+
+
+time03 = time.time()
+print(f"time03: {time03 - time02}")
+
 timestr = time.strftime("%Y%m%d-%H%M%S")
 output_file = f"mlout{timestr}.csv"
+
+time1 = time.time()
+print(f"time1: {time1 - time02}")
 # test_file = "ml data - full - biased - testSet1.csv" # 10 rows cleaned
-# predict_and_merge(test_file, output_file, sess)
+test_file = "~/mldata/testSet.csv"  # 1000 rows : MOD_CAP | TRAIN/category FULL_CAP | 150 1s, 750 0s
 
+time11 = time.time()
+print(f"time11: {time11 - time1}")
+predict_and_merge(test_file, output_file, sess)
 
+time12 = time.time()
+print(f"time0: {time12 - time11}")
+# Testing Samples
 sample = "signed and dated 🔥ends when I call it 💎💎Starts at $5 and $5 usd min increments ( no reserve ) 🔥🔥Free shipping in the US ($10 usd to Mexico/ Canada ) 🔥💎please tag the person you outbid 💎failure to pay within 24 hours of winning auction or erasing bids = block 💎 thanks for all the support. Good luck 🍀👍🏻 #rasetglass #glassart #glassauction  #glassofig #glass #glassofig #glassforsale #glassart #glassblowing #glass_of_ig #pendysofig #pendys"
 sample2 = 'Custom hand burned Shogun display box!!!💮🏯🎏🎋⛩ NFS #woodburning #colorado #boulder #woodencass #pine #woodwork #wood #japeneses #scarab #shogun #satisfying #woodart #handcarved #japeneseglass #woodworking #woodcarving #workshop #bestofglass #love #pin #katakana #woodcase #case #displaycase #engraving #dremel #woodartist #headyart #japanesestyle #srg2019'
 sample = pre_proc_text(sample)
-print(f"sample: {type(sample)}")
 sample = remove_stopwords(sample, stop_words)
 sample = ' '.join(sample)
 sample = sample.rstrip()
-print(f"sample: {type(sample)}")
-print(sample)
 sample = [sample]
-print(sample)
 predict(sample, sess)
 
-# test_file = "testSet.csv" # 1000 rows : full cap | mod caption | category 150 1s, 750 0s
-    # with open(test_file, 'r') as csvfile:
-    #     reader = csv.reader(csvfile)
-    #     for row in reader:
-    #         predict_tf_keras(row[2], sess)
+time2 = time.time()
+print(f"time2: {time2 - time12}")
+print(f"time total: {time2 - time0}")
